8bc277afbda42bd9ee7eda4a6b6ce3e0a3b766bb
asalas1093 <asalas@e112-m-05.mchenry.edu>
2015-09-24 16:50:24 -0500

    AlejandroSalas_Clock

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let now = NSDate()
+print(now)
+
+
+//declare variables
+var seconds = 1
+
+var min = 60
+
+var hour = 3600
+
+var days = 86400
+
+
+while seconds <= min {
+    ++seconds
+    println(now)
+}
+
+
+
+
+
+
+
+/*
+
+create a do while loop that will count the time and output the correct time
+
+*/
